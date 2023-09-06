3f1050523070e84a13b13cc5acbbee5ca497f504
s7tech <srikanths7tec@gmail.com>
2023-09-06 16:35:01 +0530

    added changes

--- src/pages/tours-tournaments/ToursManagement.jsx
+++ src/pages/tours-tournaments/ToursManagement.jsx
@@ -1,11 +1,49 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./style.css";
+import AddTours from "./AddTours";
+import Schedule from "./Schedule";
 
 function ToursManagement() {
+  const [activeHead, setActiveHead] = useState("add");
+  const handleToursHead = (item) => {
+    setActiveHead(item);
+  };
   return (
-    <div>
-        
+    <div className="tours-management-main p-3">
+      <div className="row d-flex justify-content-between">
+        <div className="col-2 d-flex justify-content-between">
+          <div
+            className={`${
+              activeHead === "add" ? "active-head" : ""
+            } medium-font tours-box  p-2  rounded-top text-center`}
+            onClick={() => handleToursHead("add")}
+          >
+            Add TT
+          </div>
+          <div
+            className={`${
+              activeHead === "schedule" ? "active-head" : ""
+            } medium-font tours-box  p-2  rounded-top text-center`}
+            onClick={() => handleToursHead("schedule")}
+          >
+            Schedule TT
+          </div>
+        </div>
+        <div className="col-2">
+          <div
+            className={`${
+              activeHead === "manage" ? "active-head" : ""
+            } medium-font tours-box  p-2  rounded-top text-center`}
+            onClick={() => handleToursHead("manage")}
+          >
+            Manage Tours
+          </div>
+        </div>
+      </div>
+      {activeHead === "add" && <AddTours />}
+      {activeHead === "schedule" && <Schedule />}
     </div>
-  )
+  );
 }
 
-export default ToursManagement
+export default ToursManagement;
