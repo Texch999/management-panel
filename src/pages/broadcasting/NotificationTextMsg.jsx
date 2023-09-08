3e423c5bcc0d1d24ce85ef19d11abc7ad91c86fa
sangram96rath <skesharirath96@gmail.com>
2023-09-08 15:49:50 +0530

    changes

--- src/pages/broadcasting/NotificationTextMsg.jsx
+++ src/pages/broadcasting/NotificationTextMsg.jsx
@@ -3,6 +3,8 @@ import { Button } from "react-bootstrap";
 import CreateBroadCast from "./CreateBroadCast";
 import TextMessage from "./TextMessage";
 import PostersAds from "./PostersAds";
+import { MdOutlineArrowBackIosNew } from "react-icons/md";
+import { useNavigate } from "react-router-dom";
 
 function NotificationTextMsg() {
   const headingList = [
@@ -10,32 +12,43 @@ function NotificationTextMsg() {
     "Notification and Promotion Text Message",
     "Posters & Ads",
   ];
-  const [activeReport, setActiveReport] = useState("");
+  const [activeReport, setActiveReport] = useState("Posters & Ads");
   const handleReport = (report) => {
     setActiveReport(report);
   };
+  const navigate = useNavigate();
   return (
     <div className="p-4 w-100">
       <div className="sidebar-bg rounded">
-        <div className=" d-flex justify-content-around">
-          {headingList.map((report, index) => (
-            <Button
-              key={index}
-              className={`me-2 admin-reports-button ${
-                report === activeReport
-                  ? "active text-white col-md-auto medium-font justify-content-between p-2 px-4 m-1"
-                  : "deactive-button col-md-auto medium-font justify-content-between p-2 px-4 m-1"
-              }`}
-              onClick={() => handleReport(report)}
-            >
-              {report}
-            </Button>
-          ))}
+        <div className="button-bg w-100">
+          <div className=" d-flex justify-content-around  align-items-center w-75">
+            <div className="d-flex justify-content-center">
+              <MdOutlineArrowBackIosNew
+                className="upload-icon"
+                onClick={() => navigate("/broadcasting")}
+              />
+            </div>
+            {headingList.map((report, index) => (
+              <div
+                key={index}
+                className={`me-2 ${
+                  report === activeReport
+                    ? "active-button  justify-content-between p-2 px-4 rounded-top"
+                    : "deactive-msg-button justify-content-between p-2 px-4 rounded-top"
+                }`}
+                onClick={() => handleReport(report)}
+              >
+                {report}
+              </div>
+            ))}
+          </div>
         </div>
-        <h1>Sangram</h1>
-        {activeReport === 0 && <CreateBroadCast />}
-        {activeReport === 1 && <TextMessage />}
-        {activeReport === 2 && <PostersAds />}
+
+        {activeReport === "Create New Broadcast" && <CreateBroadCast />}
+        {activeReport === "Notification and Promotion Text Message" && (
+          <TextMessage />
+        )}
+        {activeReport === "Posters & Ads" && <PostersAds />}
       </div>
     </div>
   );
