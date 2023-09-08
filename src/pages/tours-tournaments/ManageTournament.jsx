6315dfdf29e499ed8a16b3a69d6d66f7707c8f7c
s7tech <srikanths7tec@gmail.com>
2023-09-08 19:14:59 +0530

    added changes

--- src/pages/tours-tournaments/ManageTournament.jsx
+++ src/pages/tours-tournaments/ManageTournament.jsx
@@ -1,4 +1,8 @@
 import React, { useState } from "react";
+import Table from "../table/Table";
+import DocTable from "../table/DocTable";
+import { BsArrowDown } from "react-icons/bs";
+import { MdInsertPhoto } from "react-icons/md";
 
 function ManageTournament() {
   const [activeManageIndex, setActiveManageIndex] = useState(0);
@@ -8,6 +12,487 @@ function ManageTournament() {
     "Book Now",
     "Receive Document/Payment",
     "Confirm Booking List",
+  ];
+  const manageDropdown = [
+    {
+      head: "Tours",
+      options: ["All", "Tour", "Cricket Tour", "Sports Tour", "Casino TOur"],
+    },
+    {
+      head: "Website",
+      options: ["All", "w2Call", "texch.com", "ravanna.com"],
+    },
+    {
+      head: "Location",
+      options: ["All", "Hongkong", "Goa", "Srilanka", "Japan", "Thailand"],
+    },
+    {
+      head: "Name",
+      options: ["All", "jay", "vinod", "ravi", "babu"],
+    },
+    {
+      head: "Role",
+      options: [
+        "All",
+        "Director",
+        "Super Admin",
+        "Sub Admin",
+        "Admin",
+        "Super Master",
+        "Master",
+        "Agent",
+      ],
+    },
+  ];
+
+  const tableHeading = [
+    {
+      header: "S NO",
+      field: "sl",
+    },
+    {
+      header: "WEBSITE",
+      field: "website",
+    },
+    {
+      header: "DATE & TIME",
+      field: "dateTime",
+    },
+    {
+      header: "NAME",
+      field: "name",
+    },
+    {
+      header: "ROLE",
+      field: "role",
+    },
+    {
+      header: "TOURS",
+      field: "tours",
+    },
+    {
+      header: "SCHEDULE",
+      field: "schedule",
+    },
+    {
+      header: "LOCATION",
+      field: "location",
+    },
+    {
+      field: "clr",
+      clr: true,
+    },
+  ];
+
+  const tableData = [
+    {
+      sl: 1,
+      website: "we2call.com",
+      dateTime: (
+        <div>
+          31/08/2023
+          <br />
+          14:51:20 PM
+        </div>
+      ),
+      name: (
+        <div>
+          Srinivas
+          <br />
+          Hyderabad
+        </div>
+      ),
+      role: "Super Admin",
+      tours: "Sports Tours",
+      schedule: "31/08/2023",
+      location: "Dubai",
+      clr: "Select",
+    },
+    {
+      sl: 2,
+      website: "we2call.com",
+      dateTime: (
+        <div>
+          31/08/2023
+          <br />
+          14:51:20 PM
+        </div>
+      ),
+      name: (
+        <div>
+          Srinivas
+          <br />
+          Hyderabad
+        </div>
+      ),
+      role: "Super Admin",
+      tours: "Sports Tours",
+      schedule: "31/08/2023",
+      location: "Dubai",
+      clr: "Select",
+    },
+    {
+      sl: 3,
+      website: "we2call.com",
+      dateTime: (
+        <div>
+          31/08/2023
+          <br />
+          14:51:20 PM
+        </div>
+      ),
+      name: (
+        <div>
+          Srinivas
+          <br />
+          Hyderabad
+        </div>
+      ),
+      role: "Super Admin",
+      tours: "Sports Tours",
+      schedule: "31/08/2023",
+      location: "Dubai",
+      clr: "Select",
+    },
+    {
+      sl: 4,
+      website: "we2call.com",
+      dateTime: (
+        <div>
+          31/08/2023
+          <br />
+          14:51:20 PM
+        </div>
+      ),
+      name: (
+        <div>
+          Srinivas
+          <br />
+          Hyderabad
+        </div>
+      ),
+      role: "Super Admin",
+      tours: "Sports Tours",
+      schedule: "31/08/2023",
+      location: "Dubai",
+      clr: "Select",
+    },
+    {
+      sl: 4,
+      website: "we2call.com",
+      dateTime: (
+        <div>
+          31/08/2023
+          <br />
+          14:51:20 PM
+        </div>
+      ),
+      name: (
+        <div>
+          Srinivas
+          <br />
+          Hyderabad
+        </div>
+      ),
+      role: "Super Admin",
+      tours: "Sports Tours",
+      schedule: "31/08/2023",
+      location: "Dubai",
+      clr: "Select",
+    },
+  ];
+
+  const tableDocHeading = [
+    {
+      header: "S NO",
+      field: "sl",
+    },
+    {
+      header: "WEBSITE",
+      field: "website",
+    },
+    {
+      header: "DATE & TIME",
+      field: "dateTime",
+    },
+    {
+      header: "NAME & ROLE",
+      field: "nameRole",
+    },
+    {
+      header: "GUEST NAME",
+      field: "guestName",
+    },
+    {
+      header: "GENDER",
+      field: "gender",
+    },
+    {
+      header: "TOUR PACK",
+      field: "tourPack",
+    },
+    {
+      header: "TOTAL AMOUNT",
+      field: "tatalAmount",
+    },
+    {
+      header: "PAID AMOUNT",
+      field: "paidAmount",
+    },
+    {
+      field: "clr",
+    },
+  ];
+
+  const tableDocData = [
+    {
+      sl: 1,
+      website: "we2call.com",
+      dateTime: (
+        <div>
+          31/08/2023
+          <br />
+          14:51:20 PM
+        </div>
+      ),
+      nameRole: (
+        <div>
+          Srinivas
+          <br />
+          Super Admin
+          <br />
+          Hyderabad
+        </div>
+      ),
+      guestName: (
+        <div>
+          10 <BsArrowDown />
+        </div>
+      ),
+      gender: "Male",
+      tourPack: "jej",
+      tatalAmount: "31/08/2023",
+      paidAmount: (
+        <div>
+          10000
+          <br />
+          <div className="p-2 download-div">
+            id Proof
+            <MdInsertPhoto className="ms-1 ions-clr"/>
+            <BsArrowDown className="ms-1 ions-clr"/>
+          </div>
+        </div>
+      ),
+      clr: (
+        <div>
+          Confirm
+          <div className="p-2 download-div">
+            Payment
+            <MdInsertPhoto className="ms-1 ions-clr"/>
+            <BsArrowDown className="ms-1 ions-clr"/>
+          </div>
+        </div>
+      ),
+    },
+    {
+      sl: 2,
+      website: "we2call.com",
+      dateTime: (
+        <div>
+          31/08/2023
+          <br />
+          14:51:20 PM
+        </div>
+      ),
+      nameRole: (
+        <div>
+          Srinivas
+          <br />
+          Super Admin
+          <br />
+          Hyderabad
+        </div>
+      ),
+      guestName: (
+        <div>
+          10 <BsArrowDown />
+        </div>
+      ),
+      gender: "Male",
+      tourPack: "jej",
+      tatalAmount: "31/08/2023",
+      paidAmount: (
+        <div>
+          10000
+          <br />
+          <div className="p-2 download-div">
+            id Proof
+            <MdInsertPhoto className="ms-1 ions-clr"/>
+            <BsArrowDown className="ms-1 ions-clr"/>
+          </div>
+        </div>
+      ),
+      clr: (
+        <div>
+          Confirm
+          <div className="p-2 download-div">
+            Payment
+            <MdInsertPhoto className="ms-1 ions-clr"/>
+            <BsArrowDown className="ms-1 ions-clr"/>
+          </div>
+        </div>
+      ),
+    },
+    ,
+    {
+      sl: 3,
+      website: "we2call.com",
+      dateTime: (
+        <div>
+          31/08/2023
+          <br />
+          14:51:20 PM
+        </div>
+      ),
+      nameRole: (
+        <div>
+          Srinivas
+          <br />
+          Super Admin
+          <br />
+          Hyderabad
+        </div>
+      ),
+      guestName: (
+        <div>
+          10 <BsArrowDown />
+        </div>
+      ),
+      gender: "Male",
+      tourPack: "jej",
+      tatalAmount: "31/08/2023",
+      paidAmount: (
+        <div>
+          10000
+          <br />
+          <div className="p-2 download-div">
+            id Proof
+            <MdInsertPhoto className="ms-1 ions-clr"/>
+            <BsArrowDown className="ms-1 ions-clr"/>
+          </div>
+        </div>
+      ),
+      clr: (
+        <div>
+          Confirm
+          <div className="p-2 download-div">
+            Payment
+            <MdInsertPhoto className="ms-1 ions-clr"/>
+            <BsArrowDown className="ms-1 ions-clr"/>
+          </div>
+        </div>
+      ),
+    },
+    ,
+    {
+      sl: 4,
+      website: "we2call.com",
+      dateTime: (
+        <div>
+          31/08/2023
+          <br />
+          14:51:20 PM
+        </div>
+      ),
+      nameRole: (
+        <div>
+          Srinivas
+          <br />
+          Super Admin
+          <br />
+          Hyderabad
+        </div>
+      ),
+      guestName: (
+        <div>
+          10 <BsArrowDown />
+        </div>
+      ),
+      gender: "Male",
+      tourPack: "jej",
+      tatalAmount: "31/08/2023",
+      paidAmount: (
+        <div>
+          10000
+          <br />
+          <div className="p-2 download-div">
+            id Proof
+            <MdInsertPhoto className="ms-1 ions-clr"/>
+            <BsArrowDown className="ms-1 ions-clr"/>
+          </div>
+        </div>
+      ),
+      clr: (
+        <div>
+          Confirm
+          <div className="p-2 download-div">
+            Payment
+            <MdInsertPhoto className="ms-1 ions-clr"/>
+            <BsArrowDown className="ms-1 ions-clr"/>
+          </div>
+        </div>
+      ),
+    },
+    ,
+    {
+      sl: 5,
+      website: "we2call.com",
+      dateTime: (
+        <div>
+          31/08/2023
+          <br />
+          14:51:20 PM
+        </div>
+      ),
+      nameRole: (
+        <div>
+          Srinivas
+          <br />
+          Super Admin
+          <br />
+          Hyderabad
+        </div>
+      ),
+      guestName: (
+        <div>
+          10 <BsArrowDown />
+        </div>
+      ),
+      gender: "Male",
+      tourPack: "jej",
+      tatalAmount: "31/08/2023",
+      paidAmount: (
+        <div>
+          10000
+          <br />
+          <div className="p-2 download-div">
+            id Proof
+            <MdInsertPhoto className="ms-1 ions-clr"/>
+            <BsArrowDown className="ms-1 ions-clr"/>
+          </div>
+        </div>
+      ),
+      clr: (
+        <div>
+          Confirm
+          <div className="p-2 download-div">
+            Payment
+            <MdInsertPhoto className="ms-1 ions-clr"/>
+            <BsArrowDown className="ms-1 ions-clr"/>
+          </div>
+        </div>
+      ),
+    },
   ];
 
   const handleManageHead = (index) => {
@@ -33,6 +518,33 @@ function ManageTournament() {
             );
           })}
         </div>
+      </div>
+      <div className="row mt-3">
+        {manageDropdown.map((item, index) => {
+          return (
+            <div className="col" key={index}>
+              <div className="font-grey">{item.head}</div>
+              <select className="tours-box p-2 medium-font rounded-top text-center w-100">
+                {item.options.map((items, i) => {
+                  return <option key={i}>{items}</option>;
+                })}
+              </select>
+            </div>
+          );
+        })}
+      </div>
+      <div className="mt-3">
+        {(activeManageIndex === 0 ||
+          activeManageIndex === 1 ||
+          activeManageIndex === 2) && (
+          <Table columns={tableHeading} data={tableData} />
+        )}
+        {activeManageIndex === 3 && (
+          <DocTable columns={tableDocHeading} data={tableDocData} />
+        )}
+        {activeManageIndex === 4 && (
+          <DocTable columns={tableHeading} data={tableData} />
+        )}
       </div>
     </div>
   );
