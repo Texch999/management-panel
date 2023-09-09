be3a6bef39df00df3dfe90404228406072384ed3
s7tech <srikanths7tec@gmail.com>
2023-09-09 18:07:00 +0530

    added changes

--- src/pages/home/Header.jsx
+++ src/pages/home/Header.jsx
@@ -5,10 +5,21 @@ import { BiSearch } from "react-icons/bi";
 function Header() {
   return (
     <div className="header-div containaer-fluid d-flex align-items-center p-3">
-      <div className="input-group  mt-3 w-75">
-  <span className="input-group-text sidebar-bg serch-text-color" id="basic-addon1"><BiSearch/></span>
-  <input type="text" className="form-control sidebar-bg serch-text-color" placeholder="Search" aria-label="Username" aria-describedby="basic-addon1"/>
-</div>
+      <div className="input-group mb-2 mt-2 w-90">
+        <span
+          className="input-group-text sidebar-bg serch-text-color"
+          id="basic-addon1"
+        >
+          <BiSearch />
+        </span>
+        <input
+          type="text"
+          className="form-control sidebar-bg serch-text-color"
+          placeholder="Search"
+          aria-label="Username"
+          aria-describedby="basic-addon1"
+        />
+      </div>
     </div>
   );
 }
