452dcfe066de14a1a55a7f4f26bdd912b1a66a95
Bhargavi549 <bhargavidora222@gmail.com>
2023-12-02 17:58:26 +0530

    changes added

--- src/pages/referenceData/Securityquestions.jsx
+++ src/pages/referenceData/Securityquestions.jsx
@@ -10,22 +10,12 @@ import { MdOutlineEdit } from "react-icons/md";
 function Securityquestions() {
   const [allQuestions, setAllQuestions] = useState([]);
   const [selectedQuestion, setSelectedQuestion] = useState("");
-  const [filteredQuestions, setFilteredQuestions] = useState([]);
   const [selectedOption, setSelectedOption] = useState("Active");
-  const [searchText, setSearchText] = useState("");
   const [status, setStatus] = useState(false);
 
   const handleSelectChange = (event) => {
     const selectedValue = event.target.value;
     setSelectedOption(selectedValue);
-  };
-
-  const searchContent = (value) => {
-    setSearchText(value);
-    const filteredSearchText = allQuestions.filter((res) =>
-      res?.question?.toLowerCase().includes(searchText.toLowerCase())
-    );
-    setFilteredQuestions(filteredSearchText);
   };
   const cols = [
     {
@@ -56,64 +46,34 @@ function Securityquestions() {
       })
       .catch((err) => console.log(err));
   };
-  console.log("allQuestions",allQuestions);
+
+  console.log("allQuestions", allQuestions);
+
   useEffect(() => {
     getAllSecurityQuestions();
   }, [status]);
 
-  const modifiedSecurityquestionsDetails = searchText.length
-    ? filteredQuestions
-        .filter((item) =>
-          selectedOption === "Active"
-            ? item?.is_active === 1
-            : item?.is_active === 0
-        )
-        .filter((item) =>
-          item?.question?.toLowerCase().includes(searchText.toLowerCase())
-        )
-        .map((item) => {
-          return {
-            questions: <div className="role-color">{item?.question}</div>,
-            status:
-              item?.is_active === 1 ? (
-                <div className="font-green custom-active-button px-2">
-                  Active
-                </div>
-              ) : (
-                <div className="custom-deactive-button px-2">InActive</div>
-              ),
-            icon: <MdOutlineEdit className="eye-icon-size" />,
-          };
-        })
-    : allQuestions
-        .filter((item) =>
-          selectedOption === "Active"
-            ? item?.is_active === 1
-            : item?.is_active === 0
-        )
-        .map((item) => {
-          return {
-            questions: <div className="role-color">{item?.question}</div>,
-            status:
-              item?.is_active === 1 ? (
-                <div className="font-green custom-active-button px-2">
-                  Active
-                </div>
-              ) : (
-                <div className="custom-deactive-button px-2">InActive</div>
-              ),
-            icon: (
-              <MdOutlineEdit
-                className="eye-icon-size"
-                onClick={() => {
-                  console.log("testetestste");
-                  setSelectedQuestion(item);
-                  handleRejectionPopupOpen();
-                }}
-              />
-            ),
-          };
-        });
+  const modifiedSecurityquestionsDetails = allQuestions.map((item) => {
+    return {
+      questions: <div className="role-color">{item?.question}</div>,
+      status:
+        item?.is_active === "Active" ? (
+          <div className="font-green custom-active-button px-2">Active</div>
+        ) : (
+          <div className="custom-deactive-button px-2">InActive</div>
+        ),
+      icon: (
+        <MdOutlineEdit
+          className="eye-icon-size"
+          onClick={() => {
+            console.log("testetestste");
+            setSelectedQuestion(item);
+            handleRejectionPopupOpen();
+          }}
+        />
+      ),
+    };
+  });
   const [rejectPopupOpen, SetRejectpopupOpen] = useState(false);
 
   const handleRejectionPopupOpen = () => {
@@ -124,18 +84,7 @@ function Securityquestions() {
       <div className="d-flex align-items-center justify-content-between">
         <h6 className=" h6 font-grey px-2 p-2 m-1">Security Questions</h6>
         <div className=" d-flex justify-content-end align-items-center">
-          <div className="containaer-fluid px-2 w-30">
-            <form className="d-flex" role="search">
-              <input
-                className="search-width p-2 text-white w-100 sidebar-bg borderr rounded medium-font"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-                value={searchText}
-                onChange={(e) => searchContent(e.target.value)}
-              />
-            </form>
-          </div>
+          <div className="containaer-fluid px-2 w-30"></div>
           <div className="row justify-content-md-center m-2 p-1">
             <div
               onClick={() => handleRejectionPopupOpen()}
@@ -155,7 +104,6 @@ function Securityquestions() {
             <select
               className="form-select-option w-100 rounded p-2 px-3 m-1 mx-2 small-font"
               aria-label="Default select example"
-              value={selectedOption}
               onChange={handleSelectChange}
             >
               <option value="Active">Active</option>
