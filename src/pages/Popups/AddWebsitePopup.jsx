57586a309afa145df52a4181665c83f65bd82fbe
Bhargavi549 <bhargavidora222@gmail.com>
2023-12-02 19:23:39 +0530

    changes added in addwebsite popup

--- src/pages/Popups/AddWebsitePopup.jsx
+++ src/pages/Popups/AddWebsitePopup.jsx
@@ -24,6 +24,13 @@ function AddWebsitePopup(props) {
   const handleCreateWebsite = async () => {
     try {
       const res = await call(ADD_WEBSITE,formData);
+      if (res.data.status === 200) {
+        setAcceptClick(true);
+        setTimeout(() => {
+          setAcceptClick(false);
+          setShowAddWebPopup(false);
+        }, 2000);
+      }
       if (res.data.error) {
         console.error("API Error:", res.data.message);
       } else {
