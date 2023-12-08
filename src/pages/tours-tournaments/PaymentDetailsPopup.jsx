e33138b8e64be14374ff4f5e74515a3bcca59e52
JYOTHIBABU1248 <jyothibabuelubandi307@gmail.com>
2023-12-08 23:30:42 +0530

    changes added

--- src/pages/tours-tournaments/PaymentDetailsPopup.jsx
+++ src/pages/tours-tournaments/PaymentDetailsPopup.jsx
@@ -14,8 +14,12 @@ function PaymentDetailsPopup(props) {
     transactionData = {},
     isProcessing,
     handleSuccessfullPopup,
+    guestDetails
   } = props;
+  const [updatePayment, setUpdatePayment] = useState({})
   const [showScreenshotImg, setShowScreenshotImg] = useState(false);
+  console.log(updatePayment,'.....updatepayment')
+
   const handleShowImg = () => {
     setShowScreenshotImg(true);
   };
@@ -24,6 +28,8 @@ function PaymentDetailsPopup(props) {
     setShowPaymentDetailsPopup(false);
   };
   const [acceptClick, setAcceptClick] = useState(false);
+  const paymentDetails = guestDetails.payment_details
+  console.log(paymentDetails,'......paymentdetails')
 
   const getAllRejections = async () => {
     const payload = {
@@ -39,6 +45,19 @@ function PaymentDetailsPopup(props) {
     getAllRejections();
   }, []);
 
+  const handleOnchange = (e) => {
+    setUpdatePayment({
+        ...updatePayment,
+        [e.target.name] : e.target.value
+    })
+  }
+  const handleOnclick = (val)  =>{
+    setUpdatePayment({
+        ...updatePayment,
+        confirmationStatus : val
+    })
+  }
+
   return (
     <div className="modal fade bd-example-modal-lg container mt-5">
       <Modal
@@ -52,50 +71,49 @@ function PaymentDetailsPopup(props) {
           <div className="w-100 p-4 flex-columnn relative-position">
             <div className="d-flex justify-content-start py-2">
               <div className="w-25 small-font d-flex align-items-center ">
-                Srinivas
+                {guestDetails.name}
                 <button className="yellow-btn rounded clr-orange mx-2 px-2 py-1">
-                  Director
+                {guestDetails.account_role}
                 </button>
                 <div className="rounded-pill small-font px-3 py-1 completed-btn">
                   Deposit
                 </div>
+                <div className="rounded-pill small-font px-3 py-1 completed-btn">
+                {guestDetails.website}
+                </div>
               </div>
             </div>
-            {/* <div className="d-flex flex-row w-100 custom-select small-font input-btn-bg rounded align-items-center justify-content-between my-2 px-2 py-2">
-              <div className="font-grey">Trx ID</div>
-              <div>{transactionData?.transaction_id}</div>
-            </div>
-            <div className="d-flex flex-row w-100 custom-select small-font input-btn-bg rounded align-items-center justify-content-between my-2 px-2 py-2">
-              <div className="font-grey">Reference ID</div>
-              <div>{transactionData?.package_requester_id}</div>
-            </div> */}
             <div className="d-flex flex-column px-2 input-btn-bg rounded">
               <div className="d-flex flex-row justify-content-between small-font  all-none  align-items-center justify-content-between my-1 ">
                 <div className="font-grey">Amount To Be Paid</div>
-                <div className="font-grey">100000</div>
+                <div className="font-grey">{guestDetails.total_amount}</div>
               </div>
               <div className="d-flex flex-row justify-content-between small-font  all-none  align-items-center justify-content-between my-1 ">
                 <div className="font-grey">Payment Method</div>
-                <div className="font-grey">UPI</div>
+                <div className="font-grey">{guestDetails.payment_details.paymentmode}</div>
               </div>
               <div className="d-flex flex-row justify-content-between small-font  all-none  align-items-center justify-content-between my-1 ">
-                <div className="font-grey">From</div>
-                <div className="font-grey">username</div>
+                <div className="font-grey">Paid To</div>
+                <div className="font-grey">{guestDetails.payment_details.name}</div>
               </div>
               <div className="d-flex flex-row justify-content-between small-font  all-none  align-items-center justify-content-between my-1 ">
-                <div className="font-grey">To</div>
-                <div className="font-grey">username</div>
-                {/* <div>Jayanta-Admin</div> */}
+                <div className="font-grey">{guestDetails.payment_details.paymentmode==="neft"?"Account No":"Mobile Number"}</div>
+                <div className="font-grey">
+                    {guestDetails.payment_details.paymentmode==="neft"?
+                    guestDetails.payment_details.accountNo : guestDetails.payment_details.mobileNumber}</div>
               </div>
-              {/* <div className="d-flex flex-row justify-content-between small-font  all-none  align-items-center justify-content-between my-2 "> */}
-              {/* <div className="font-grey">Time</div> */}
-              {/* <div className="font-grey">{moment(requestedPackages?.created_date).format("DD-MM-yyyy")}{" "}
-              {requestedPackages?.created_time}</div> */}
-              {/* <div>
-                  {transactionData?.created_date}{" "}
-                  {transactionData?.created_time}
-                </div> */}
-              {/* </div> */}
+              <div className="d-flex flex-row justify-content-between small-font  all-none  align-items-center justify-content-between my-1 ">
+                <div className="font-grey">{guestDetails.payment_details.paymentmode==="neft"?"IFSC Code":"UPI Id"}</div>
+                <div className="font-grey">
+                    {guestDetails.payment_details.paymentmode==="neft"?
+                    guestDetails.payment_details.ifscCode : guestDetails.payment_details.upiId}
+                </div>
+              </div>
+              {guestDetails.payment_details.paymentmode==="neft" && 
+              (<div className="d-flex flex-row justify-content-between small-font  all-none  align-items-center justify-content-between my-1 ">
+                <div className="font-grey">Bank</div>
+                <div className="font-grey">{guestDetails.payment_details.bank}</div>
+              </div>)}
             </div>
             <div className="w-100 my-2 relative-position">
               <img
@@ -114,140 +132,30 @@ function PaymentDetailsPopup(props) {
                 type="text"
                 placeholder="Enter Amount Paid"
                 className="w-75 custom-select small-font input-btn-bg px-2 py-2 all-none rounded all-none my-1"
+                name="amountPaid"
+                value={updatePayment.amountPaid || ""}
+                onChange={(e)=>handleOnchange(e)}
               ></input>
             </div>
             <div className="d-flex flex-row justify-content-between small-font  all-none  align-items-center justify-content-between my-2 ">
-              <button className="btn btn-success btn-sm">Approve</button>
-              <button className="btn btn-danger btn-sm">Reject</button>
+              <button className="btn btn-success btn-sm"
+                      name="confirmationStatus"
+                      onClick={()=>handleOnclick("Approve")}
+              >Approve</button>
+              <button className="btn btn-danger btn-sm"
+                      name="confirmationStatus"
+                      onClick={()=>handleOnclick("Reject")}
+              >Reject</button>
             </div>
-            {/* <Container fluid className="my-2 w-100">
-              <Row>
-                <Col className="ps-0 col-12">
-                  <Dropdown
-                    size="lg"
-                    className="user-dropdown-toggle custom-button-drop small-font mt-2"
-                  >
-                    <Dropdown.Toggle className="w-100">
-                      <div className=" w-100 d-flex align-itens-center justify-content-between p-1">
-                        <div className="font-grey">Package List</div>
-                        <span style={{ float: "right" }} className="clr-yellow">
-                          View
-                        </span>
-                      </div>
-                    </Dropdown.Toggle>
-                    <Dropdown.Menu className="custom-menu-item px-1">
-                      <table className="w-100 match-position-table small-font clr-white">
-                        <thead>
-                          <tr className="text-center">
-                            <td>Package</td>
-                            <td>Purchase</td>
-                            <td>Price</td>
-                            <td>Return Pkg</td>
-                            <td>Return Hrs</td>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          {tableData?.map((item, index) => (
-                            <tr key={index} className="text-center">
-                              <td>{item.packages}</td>
-                              <td>{item.purchase}</td>
-                              <td>{item.price}</td>
-                              <td>{item.returnpkg}</td>
-                              <td>{item.returnhrs}</td>
-                            </tr>
-                          ))}
-                          {returnPackages?.map((item, index) => (
-                            <tr key={index} className="text-center">
-                              <td>{item.packages}</td>
-                              <td>{item.purchase}</td>
-                              <td>{item.price}</td>
-                              <td>{item.returnpkg}</td>
-                              <td>{item.returnhrs}</td>
-                            </tr>
-                          ))}
-                        </tbody>
-                        <tfoot className="small-font justify-content-between w-100"></tfoot>
-                      </table>
-                      <Dropdown.Item className="rounded my-2 d-flex flex-row ps-0 pe-0">
-                        <div className="d-flex flex-row w-100 justify-content-between small-font">
-                          <div className="d-flex flex-column w-50 mx-2 my-2">
-                            <div className="d-flex flex-row justify-content-between">
-                              <span>Total Purchase</span>
-                              <span>
-                                {totalNoPackages}= {totalNoPackagesCost}
-                              </span>
-                            </div>{" "}
-                            <div className="d-flex flex-row justify-content-between">
-                              <span>Return Pkg</span>
-                              <span>
-                                {totalNoReturnPackages} ={" "}
-                                {totalNoReturnPackagesCost}
-                              </span>
-                            </div>{" "}
-                            <div className="d-flex flex-row justify-content-between">
-                              <span>Return Hrs</span>
-                              <span>26 = 30000</span>
-                            </div>
-                          </div>{" "}
-                          <div className="d-flex flex-column w-50 mx-2 my-2">
-                            <div className="d-flex flex-row justify-content-between">
-                              <span>Discount</span>
-                              <span>
-                                {totalPackagesDiscount}% ={" "}
-                                {afterPackDiscountTotalCost}
-                              </span>
-                            </div>{" "}
-                            <div className="d-flex flex-row justify-content-between">
-                              <span>Special Offer</span>
-                              <span>
-                                {transactionData?.summary?.user_discount}% ={" "}
-                                {userDiscountCost}
-                              </span>
-                            </div>{" "}
-                            <div className="d-flex flex-row justify-content-between">
-                              <span>Paid Amount</span>
-                              <span>
-                                {transactionData?.summary?.final_package_cost}
-                              </span>
-                            </div>{" "}
-                          </div>
-                        </div>
-                      </Dropdown.Item>
-                    </Dropdown.Menu>
-                  </Dropdown>
-                </Col>
-                <Col className="pe-0">
-                  <Dropdown
-                    size="lg"
-                    className="user-dropdown-toggle custom-button-drop small-font mt-2"
-                  ></Dropdown>
-                </Col>
-              </Row>
-            </Container> */}
-            {/* <Container fluid className="my-2 w-100">
-              <Row>
-                <Col className="ps-0">
-                  <Dropdown
-                    size="lg"
-                    className="user-dropdown-toggle custom-button-drop small-font mt-2"
-                  ></Dropdown>
-                </Col>
-                <Col className="pe-0">
-                  <Dropdown
-                    size="lg"
-                    className="user-dropdown-toggle custom-button-drop small-font mt-2"
-                  ></Dropdown>
-                </Col>
-              </Row>
-            </Container> */}
             <div className="d-flex flex-row justify-content-between small-font  all-none  align-items-center justify-content-between ">
               <div className="font-grey">Select reason for rejection</div>
             </div>
             <div>
               <select
                 id="reason"
-                name="reason"
-                value={""}
+                name="rejectionReason"
+                value={updatePayment.rejectionReason || ""}
+                onChange={(e)=>handleOnchange(e)}
                 className="w-100 custom-select small-font input-btn-bg px-2 py-2 all-none rounded all-none my-1"
               >
                 <option value="">Select</option>
@@ -257,7 +165,6 @@ function PaymentDetailsPopup(props) {
                 ))}
               </select>
             </div>
-            {/* <hr /> */}
             <input
               type="text"
               placeholder="OR Mention Any Other Reason for Rejection"
