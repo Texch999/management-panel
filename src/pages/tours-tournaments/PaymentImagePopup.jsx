d4675577a1b3123d4ca9c496dfbaf2fb68e0394e
JYOTHIBABU1248 <jyothibabuelubandi307@gmail.com>
2024-01-08 19:12:02 +0530

    changes added

--- src/pages/tours-tournaments/PaymentImagePopup.jsx
+++ src/pages/tours-tournaments/PaymentImagePopup.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import { ModalHeader } from "react-bootstrap";
 import { Col, Container, Modal, Row } from "react-bootstrap";
-import { Document, Page } from "react-pdf";
+// import { Document, Page } from "react-pdf";
+// import { Worker, Viewer } from "@react-pdf-viewer/core";
+import { Document, Page } from 'react-pdf';
+import { Worker, Viewer } from '@react-pdf-viewer/core';
+
+import "@react-pdf-viewer/core/lib/styles/index.css";
 
 function PaymentImagePopup(props) {
   const {
@@ -12,16 +17,9 @@ function PaymentImagePopup(props) {
   } = props;
 
   console.log(documentView, "......imageurl");
-  // const fileExtensionForView = documentView?.split('.')
-  // console.log(fileExtensionForView[1],'......view')
-  const fileName = documentView?.split('/').pop();
-  
-  // Get the file extension
-  console.log(fileName,'........filename')
-  const fileExtension = fileName?.split('.').pop();
-  console.log(fileExtension,'........fileext')
-  
-  // return fileExtension;
+  const fileName = documentView?.split("/").pop();
+  const fileExtension = fileName?.split(".").pop();
+  console.log(fileExtension, "........fileext");
 
   const handlePopupClose = () => {
     setDocumentView("");
@@ -37,19 +35,18 @@ function PaymentImagePopup(props) {
         centered
         className="match-share-modal payment-modal"
       >
-        <Modal.Header closeButton>
-        </Modal.Header>
+        <Modal.Header closeButton></Modal.Header>
         {fileExtension === "pdf" ? (
-            <div>
-              <Document file={documentView}>
-                <Page pageNumber={1} />
-              </Document>
-            </div>
-          ) : (
-            <div>
-              <img src={documentView} />
-            </div>
-          )}
+          <div style={{height:"10vw", width:"10vw"}}>
+            <Document file={documentView}>
+              <Page pageNumber={1} />
+            </Document>
+          </div>
+        ) : (
+          <div>
+            <img src={documentView} />
+          </div>
+        )}
       </Modal>
     </div>
   );
