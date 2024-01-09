8f22c7b0c5c36b396c2a1c0e51cd4e75081dd454
JYOTHIBABU1248 <jyothibabuelubandi307@gmail.com>
2024-01-09 13:26:56 +0530

    changes added

--- src/pages/tours-tournaments/PaymentImagePopup.jsx
+++ src/pages/tours-tournaments/PaymentImagePopup.jsx
@@ -3,9 +3,8 @@ import { ModalHeader } from "react-bootstrap";
 import { Col, Container, Modal, Row } from "react-bootstrap";
 // import { Document, Page } from "react-pdf";
 // import { Worker, Viewer } from "@react-pdf-viewer/core";
-import { Document, Page } from 'react-pdf';
-import { Worker, Viewer } from '@react-pdf-viewer/core';
-
+import { Document, Page } from "react-pdf";
+import { Worker, Viewer } from "@react-pdf-viewer/core";
 import "@react-pdf-viewer/core/lib/styles/index.css";
 
 function PaymentImagePopup(props) {
@@ -35,12 +34,17 @@ function PaymentImagePopup(props) {
         centered
         className="match-share-modal payment-modal"
       >
-        <Modal.Header closeButton></Modal.Header>
+        <Modal.Header closeButton>
+        </Modal.Header>
         {fileExtension === "pdf" ? (
-          <div style={{height:"10vw", width:"10vw"}}>
-            <Document file={documentView}>
-              <Page pageNumber={1} />
-            </Document>
+          <div >
+            <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js">
+              <Viewer fileUrl={documentView}>
+                <Document file={documentView}>
+                  <Page pageNumber={1} />
+                </Document>
+              </Viewer>
+            </Worker>
           </div>
         ) : (
           <div>
